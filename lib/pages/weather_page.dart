c2accde4d328ea8b2261674d30c497087f275ae0
sushil d. shrestha <61751094+sushildshrestha@users.noreply.github.com>
2024-05-21 11:02:21 +0545

    first commit weather page, weather model

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WeatherPageScreen extends StatefulWidget {
+  const WeatherPageScreen({super.key});
+
+  @override
+  State<WeatherPageScreen> createState() => _WeatherPageScreenState();
+}
+
+class _WeatherPageScreenState extends State<WeatherPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
